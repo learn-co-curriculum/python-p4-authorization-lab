1c53c3359055bf3e31c1807d2c4f85f7631f533e
Ben Botsford <ben.botsford@flatironschool.com>
2023-04-10 16:56:12 -0500

    remove extra test files

---
+++ server/migrations/versions/a76dbcefe654_create_db.py
@@ -0,0 +1,24 @@
+"""create db
+
+Revision ID: a76dbcefe654
+Revises: 
+Create Date: 2022-11-15 10:32:43.094302
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a76dbcefe654'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
